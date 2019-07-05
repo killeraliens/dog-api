a503b7178a4745fbbb19ee10e35040be6bbbfcae
killeraliens <killeraliens@outlook.com>
2019-07-05 10:19:54 -0700

    Create an app that loads a single random image for a specific breed, based on a user input.

--- main.js
+++ main.js
@@ -3,28 +3,31 @@ function returnAltDescription(dogImgUrl) {
     return altText.replace(/\/.+/, "");
 }
 
-function renderDogLiString(dogImgUrl) {
-    return `<li><img class="dog-image" src="${dogImgUrl}" alt="${returnAltDescription(dogImgUrl)}"></li>`
+function imgString(imgUrl) {
+    return `<img class="dog-image" src="${imgUrl}" alt="${returnAltDescription(imgUrl)}">`
 }
 
-function displayDogListings(resultsArr) {
+function liString(imgUrl) {
+    return `<li>${imgString(imgUrl)}</li>`;
+}
+
+function displayListings(resultsArr) {
     liStringArr = [];
     resultsArr.forEach(result => {
         console.log(result);
-        liStringArr.push(renderDogLiString(result));
-    })
-    const displayUl = $('#js-show-listings');
-    displayUl.html(liStringArr.join('\n'));
-    displayUl.removeClass('hidden');
+        liStringArr.push(liString(result));
+    });
+    $('#js-display').html(`<ul>${liStringArr.join('\n')}</ul>`);
+    $('.container').removeClass('max-width');
 }
 
 function fetchMultipleDogs(qty) {
     fetch(`https://dog.ceo/api/breeds/image/random/${qty}`)
         .then(result => result.json())
         .then(resultsJson => {
-            displayDogListings(resultsJson.message);
+            displayListings(resultsJson.message);
         })
-        .catch(err => alert("something went horribly wrong"));
+        .catch(err => displayErrorMessage(err));
 }
 
 function handleQtySubmit() {
@@ -35,4 +38,49 @@ function handleQtySubmit() {
     })
 }
 
-handleQtySubmit();
+function displayBreedImage(imgUrl) {
+    $('#js-display').html(imgString(imgUrl));
+    $('.container').addClass('max-width');
+}
+
+
+function displayErrorMessage(errorStr) {
+    const errString = `<h2>${errorStr}</h2>`
+    $('#js-display').html(errString);
+}
+
+
+function fetchBreed(breed) {
+    if (breed) {
+        fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+            .then(result => result.json())
+            .then(resultJson => {
+                console.log(resultJson);
+                if (resultJson.status === "error") {
+                    displayErrorMessage(resultJson.message);
+                } else {
+                    displayBreedImage(resultJson.message);
+                    $('#js-display').prepend(`<h2>${breed}</h2>`)
+                }
+            })
+            .catch(err => displayErrorMessage(err));
+    } else {
+        displayErrorMessage("Please enter a breed");
+    }
+}
+
+
+function handleBreedSubmit() {
+    $('#js-get-breed').on('submit', function(e) {
+        e.preventDefault();
+        const breed = $('#breed').val();
+        fetchBreed(breed);
+        $('#breed').val("");
+    })
+}
+
+
+$(function() {
+    handleQtySubmit();
+    handleBreedSubmit();
+})
