4ad5deef492652b2eaf336b7f3a6d9d2c8a254f3
killeraliens <killeraliens@outlook.com>
2019-07-04 18:25:52 -0700

    Create an app that lets users choose to display between 1 and 50 random dog images, then loads the images in the console and DOM

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
-function retrieveMultipleDogs(qty) {
+function returnAltDescription(dogImgUrl) {
+    let altText = dogImgUrl.replace(/https:\/\/images.dog.ceo\/breeds\//, "");
+    return altText.replace(/\/.+/, "");
+}
+
+function renderDogLiString(dogImgUrl) {
+    return `<li><img class="dog-image" src="${dogImgUrl}" alt="${returnAltDescription(dogImgUrl)}"></li>`
+}
+
+function displayDogListings(resultsArr) {
+    liStringArr = [];
+    resultsArr.forEach(result => {
+        console.log(result);
+        liStringArr.push(renderDogLiString(result));
+    })
+    const displayUl = $('#js-show-listings');
+    displayUl.html(liStringArr.join('\n'));
+    displayUl.removeClass('hidden');
+}
+
+function fetchMultipleDogs(qty) {
     fetch(`https://dog.ceo/api/breeds/image/random/${qty}`)
         .then(result => result.json())
         .then(resultsJson => {
-            console.log(resultsJson);
-            resultsJson.message.forEach(result => console.log(result))
+            displayDogListings(resultsJson.message);
         })
-        .catch(err => alert("something went horribly wrong"))
-
+        .catch(err => alert("something went horribly wrong"));
 }
 
 function handleQtySubmit() {
     $('#js-get-qty').on('submit', function(e) {
         e.preventDefault();
         const qty = $('#qty').val();
-        retrieveMultipleDogs(qty);
+        fetchMultipleDogs(qty);
     })
-
 }
 
 handleQtySubmit();
